fcb23a6c597b5658539a03290cbcd0c071e14695
Ricardo Arevalo <1ricardoarevalo@gmail.com>
2023-09-15 07:02:08 -0500

    change layout based on landscape images

--- src/views/Cards/Components/Card.js
+++ src/views/Cards/Components/Card.js
@@ -10,21 +10,23 @@ const Card = ({ listImage, onBtnClickKit }) => {
     listImage[0].fields.imagenTarjeta[0].fields.file.url;
 
   return (
-    <div className="flex flex-col gap-10 px-5 py-8 md:px-20 lg:px-10 w-full border border-slate-100 border-t-4 border-t-green3 rounded-lg shadow-xl md:flex-row lg:flex-row">
-     <div className="w-12/12 md:8/12 lg:w-7/12">
-        <div className="text-gray-600 text-3xl leading-none font-bold">
+    <div className="flex flex-col gap-10 w-full border border-slate-100 border-t-4 border-t-green3 rounded-lg shadow-xl">
+        {hasImage && (
+      <div className="w-12/12 w-full flex items-center justify-center md:px-20 lg:px-10 bg-slate-200 rounded-sm h-48 relative bg-[conic-gradient(at_left,_var(--tw-gradient-stops))] from-yellow-50 via-green-100 to-green-500">
+        
+        <img src={listImage[0].fields.imagenTarjeta[0].fields.file.url}  className="rounded-lg w-5/12 shadow-2xl border bg-white mt-6 absolute top-0"/>
+      </div>
+    )}
+     <div className="w-12/12 px-5 py-8 md:px-20 lg:px-10 mt-20 pt-8">
+     <div className="text-green3 text-3xl leading-none font-bold">
             {listImage[0].fields.titulo}
           </div>
-          <div className="mt-4 text-gray-500 space-y-5"
+          <div className="mt-4 text-gray-500 pb-6"
           dangerouslySetInnerHTML={{ __html: listImage[0].fields.descripcion }}
         ></div>
      </div>
           
-    {hasImage && (
-      <div className="w-12/12 w-full md:4/12 lg:w-5/12 flex justify-center">
-        <img src={listImage[0].fields.imagenTarjeta[0].fields.file.url}  className="rounded-lg"/>
-      </div>
-    )}
+  
   
       
     </div>
